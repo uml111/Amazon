11e20215c58f89b80f7ebd7f2b6f3d33f8b39611
lavan <lavan@LAPTOP-3US3VUHT>
2021-10-19 14:22:48 -0700

    code updated

--- Test/src/Amazon/com/Gittest1.java
+++ Test/src/Amazon/com/Gittest1.java
@@ -1,10 +1,23 @@
 package Amazon.com;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class Gittest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+
+		
+		System.setProperty("webdriver.edge.driver","C:\\Selenium-Software\\Drivers-New\\edgedriver_win64\\msedgedriver.exe");
+		WebDriver driver1=new EdgeDriver();
+
+		driver1.get("https://www.target.com/");
+		driver1.manage().window().maximize();
+		driver1.close();
+		
 	}
 
 }
